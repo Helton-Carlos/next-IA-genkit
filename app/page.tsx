3ae7c3bd571f5e88a4e5bd439df3f8c903c6abd9
Helton-Carlos <heltoncarlospa>
2026-06-13 15:18:18 -0300

    feat: improvement text

--- app/page.tsx
+++ app/page.tsx
@@ -3,14 +3,14 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="bg-primary h-screen">
-      <main className="mx-auto w-full max-w-3xl">
+      <main className="mx-auto max-w-2xl">
         <div className="py-8 text-center">
-          <h3 className="text-2xl font-bold uppercase md:text-4xl mt-8">
-            Seja bem vindo,
+          <h3 className="text-4xl font-bold uppercase mt-8">
+            Seja bem vindo!
           </h3>
-          <p className="text-2xl font-bold text-gray-400 uppercase mb-8 md:text-4xl">ao next IA genkit.</p>
+          <p className="bg-white text-primary text-2xl font-bold uppercase mb-8 md:text-4xl">ao next IA genkit.</p>
 
-          <span className="mt-4 block text-sm text-gray-300 md:text-3xl">
+          <span className="mt-4 block text-2xl text-gray-300 md:text-3xl">
             Aprenda inglês e espanhol de forma inteligente com IA generativa powered by Genkit.
           </span>
         </div>
