f7dff047e9af4e916f6b226019d105eb7d0dc90f
Helton-Carlos <heltoncarlospa>
2026-06-13 10:51:03 -0300

    feat: add home page

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -4,7 +4,7 @@ import MobileMenu from './MobileMenu'
 
 export default function Header() {
   return (
-    <header className="sticky top-0 z-50 w-full border-b border-gray-200 bg-white backdrop-blur-md">
+    <header className="sticky top-0 z-50 w-full border-b border-gray-200 bg-white">
       <div className="mx-auto flex h-18 max-w-7xl items-center justify-between px-4 sm:px-6 lg:px-8">
         <div className="flex items-center">
           <Link href="/" className="text-xl font-bold text-gray-900 tracking-tight flex items-center gap-2">
--- app/page.tsx
+++ app/page.tsx
@@ -2,16 +2,28 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center bg-primary h-screen">
-      <main className="flex flex-1 w-full max-w-3xl">
+    <div className="bg-primary h-screen">
+      <main className="mx-auto w-full max-w-3xl">
+        <div className="py-8 text-center">
+          <h3 className="text-2xl font-bold uppercase md:text-4xl mt-8">
+            Seja bem vindo,
+          </h3>
+          <p className="text-2xl font-bold text-gray-400 uppercase mb-8 md:text-4xl">ao next IA genkit.</p>
+
+          <span className="mt-4 block text-sm text-gray-300 md:text-3xl">
+            Aprenda inglês e espanhol de forma inteligente com IA generativa powered by Genkit.
+          </span>
+        </div>
+
         <Image
           src="/home-image.svg"
-          alt="logo grande com nome next ai genkit"
+          alt="logo grande com nome next IA genkit"
           width={700}
           height={150}
           priority
+          className="w-full mx-auto max-w-[250px] sm:max-w-[350px] md:max-w-[500px] lg:max-w-[700px] h-auto"
         />
       </main>
-    </div>
+    </div >
   );
 }
