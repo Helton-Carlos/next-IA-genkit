d4ec935e658021d3d86651ac6bf5ebeb37ab8804
Helton Silva <helton.silva@melhorenvio.com>
2026-06-15 15:43:31 -0300

    feat: add submit function

--- app/form/page.tsx
+++ app/form/page.tsx
@@ -1,26 +1,38 @@
 "use client"
 
-import { useRef } from "react";
-import Link from 'next/link'
+import { useRouter } from "next/navigation" 
+import Link from "next/link"
 
 export default function FormPage() {
-  const proximoInputRef = useRef(null); 
+  const router = useRouter() 
 
-  function lidarComSelecao(evento: any) {
-     console.log(evento)
-  };
+  function onSubmitHandler(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault() 
+    
+    const formData = new FormData(e.currentTarget)
+    
+    const dados = {
+      idioma: formData.get("idioma"),
+      nivel: formData.get("nivel"),
+      nome: formData.get("nome"),
+    }
 
-  function onSubmitHandler() {
-    console.log(proximoInputRef)
+    console.log(dados)
+    router.push("/chat")
   }
 
-  return(
+  return (
     <div className="py-8 px-4">
       <form onSubmit={onSubmitHandler} className="bg-gray-300 max-w-120 text-black mx-auto p-4 rounded-md">
         <h3 className="text-2xl font-bold mb-4">Responda o formulário</h3>
+        
         <div className="flex flex-col gap-2">
           <label htmlFor="idioma">Qual idioma você quer aprender?</label>
-          <select className="bg-white p-2 rounded-md border border-gray-300 focus:border-primary focus:outline-none" onChange={lidarComSelecao}>
+          <select 
+            name="idioma" 
+            id="idioma"
+            className="bg-white p-2 rounded-md border border-gray-300 focus:border-primary focus:outline-none"
+          >
             <option value="">Selecione uma opção</option>
             <option value="english">Inglês</option>
             <option value="spanish">Espanhol</option>
@@ -28,8 +40,12 @@ export default function FormPage() {
         </div>
 
         <div className="flex flex-col my-4 gap-2">
-          <label htmlFor="idioma">Qual é seu nivel atual?</label>
-          <select className="bg-white p-2 rounded-md border border-gray-300 focus:border-primary focus:outline-none" onChange={lidarComSelecao}>
+          <label htmlFor="nivel">Qual é seu nível atual?</label>
+          <select 
+            name="nivel" 
+            id="nivel"
+            className="bg-white p-2 rounded-md border border-gray-300 focus:border-primary focus:outline-none"
+          >
             <option value="">Selecione uma opção</option>
             <option value="a-1">A-1</option>
             <option value="a-2">A-2</option>
@@ -41,12 +57,18 @@ export default function FormPage() {
         </div>
 
         <div className="flex flex-col my-4 gap-2">
-          <label htmlFor="idioma">Escreva seu apelido ou nome:</label>
-          <input type="text" placeholder="ex: João, Maria, outros..." className="bg-white p-2 rounded-md border border-gray-300 focus:border-primary focus:outline-none"  />
+          <label htmlFor="nome">Escreva seu apelido ou nome:</label>
+          <input 
+            type="text" 
+            name="nome" 
+            id="nome"
+            placeholder="ex: João, Maria, outros..." 
+            className="bg-white p-2 rounded-md border border-gray-300 focus:border-primary focus:outline-none"  
+          />
         </div>
 
         <div className="flex items-center gap-8 hover:underline">
-          <Link href="/" className="font-bold" >
+          <Link href="/" className="font-bold">
             Voltar
           </Link>
 
